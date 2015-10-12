c2a3059b502958f6361eed1ee145730f0b75b714
Marcus van Dam <marcus@marcusvandam.nl>
2015-10-12 17:10:45 +0200

    OSX (Darwin) compatibility.
    adding Darwin environment variables.
    moving default environment initialization to make room for OS specifics.

---
+++ 00_OS-Darwin.zsh
@@ -0,0 +1,16 @@
+[[ $(uname -s) == "Darwin" ]] || return
+
+BREW_COREUTILS=$(brew --prefix coreutils)
+
+if [ -d $BREW_COREUTILS ]; then
+    # let GNU coreutils installed by brew take preecedence on Mac systems
+    export PATH="${BREW_COREUTILS}/libexec/gnubin:$PATH"
+    export MANPATH="${BREW_COREUTILS}/libexec/gnuman:$MANPATH"
+else
+    # the OSX way for ls colors.
+	export CLICOLOR=1
+	# TODO: we should add an LSCOLORS here..
+fi
+
+# add homebrew's zsh-completions
+fpath=(/usr/local/share/zsh-completions $fpath)
