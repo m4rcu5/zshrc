c5d6f4610f45c487b6bc75102ec6c21b697f053e
Marcus van Dam <marcus@marcusvandam.nl>
2018-03-12 15:25:03 +0100

    Adding support for ecdsa in keychain

--- 70_Keychain.zsh
+++ 70_Keychain.zsh
@@ -4,7 +4,7 @@
 
 if [ `whoami` != 'root' ]; then
     if which keychain &> /dev/null; then
-    	keys=(~/.ssh/id_[rd]sa^*.pub)
+        keys=(~/.ssh/id_(ed2551|[rd]sa)^*.pub)
 
         keychain --inherit any-once --agents ssh --quiet $keys
         [[ -e ~/.keychain/${HOST}-sh ]] && . ~/.keychain/${HOST}-sh
