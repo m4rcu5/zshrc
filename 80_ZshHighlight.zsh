96dc5b29f085a2a65ad5911ab10206dd82fae7d8
Marcus van Dam <marcus@marcusvandam.nl>
2023-07-27 13:49:23 +0200

    I've recently changed distro's and this plugin seems to live in different places.

--- 80_ZshHighlight.zsh
+++ 80_ZshHighlight.zsh
@@ -1,34 +1,42 @@
 #
 # Online syntax highlighting
 #
-PLUGIN='/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
+PLUGIN=(
+    '/usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
+    '/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
+)
 
-if [ -f ${PLUGIN} ]; then
+for f in ${PLUGIN}; do
+    [[ -e $f ]] && PLUGIN=$f
+    break
+done
+
+if [[ -e ${PLUGIN} ]]; then
     ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
     source ${PLUGIN}
+
+    ZSH_HIGHLIGHT_STYLES[cursor]='none'
+    ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]='bg=blue'
+
+    ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=cyan,bold'
+    ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=red,bold'
+    ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=yellow,bold'
+    ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=magenta,bold'
+    ZSH_HIGHLIGHT_STYLES[bracket-level-5]='fg=green,bold'
+
+    ZSH_HIGHLIGHT_STYLES[assign]='fg=cyan'
+
+    ZSH_HIGHLIGHT_STYLES[builtin]='fg=yellow'
+    ZSH_HIGHLIGHT_STYLES[precommand]='fg=yellow'
+
+    ZSH_HIGHLIGHT_STYLES[command]='bold'
+    ZSH_HIGHLIGHT_STYLES[hashed-command]='bold'
+
+    ZSH_HIGHLIGHT_STYLES[globbing]='fg=cyan'
+    ZSH_HIGHLIGHT_STYLES[path]='bold'
+
+    ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=magenta,bold'
+    ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=magenta,bold'
 fi
-
-ZSH_HIGHLIGHT_STYLES[cursor]='none'
-ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]='bg=blue'
-
-ZSH_HIGHLIGHT_STYLES[bracket-level-1]='fg=cyan,bold'
-ZSH_HIGHLIGHT_STYLES[bracket-level-2]='fg=red,bold'
-ZSH_HIGHLIGHT_STYLES[bracket-level-3]='fg=yellow,bold'
-ZSH_HIGHLIGHT_STYLES[bracket-level-4]='fg=magenta,bold'
-ZSH_HIGHLIGHT_STYLES[bracket-level-5]='fg=green,bold'
-
-ZSH_HIGHLIGHT_STYLES[assign]='fg=cyan'
-
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=yellow'
-ZSH_HIGHLIGHT_STYLES[precommand]='fg=yellow'
-
-ZSH_HIGHLIGHT_STYLES[command]='bold'
-ZSH_HIGHLIGHT_STYLES[hashed-command]='bold'
-
-ZSH_HIGHLIGHT_STYLES[globbing]='fg=cyan'
-ZSH_HIGHLIGHT_STYLES[path]='bold'
-
-ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=magenta,bold'
-ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=magenta,bold'
 
 unset PLUGIN
